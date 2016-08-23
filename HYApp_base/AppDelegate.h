67509f21fef85b8e6fc4c52a86b9ccdb6036ef5b
castial <897257524@qq.com>
2016-08-23 16:46:24 +0800

    first commit

---
+++ HYApp_base/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYApp_base
+//
+//  Created by Hyyy on 16/8/19.
+//  Copyright © 2016年 Hyyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYApp_base/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYApp_base
+//
+//  Created by Hyyy on 16/8/19.
+//  Copyright © 2016年 Hyyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
