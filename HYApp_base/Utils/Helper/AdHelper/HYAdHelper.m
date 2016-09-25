cf6d2152b5e9b886c016b8b7ad5debe5da1ba598
castial <897257524@qq.com>
2016-09-25 22:06:07 +0800

    修改广告代码

--- HYApp_base/Utils/Helper/AdHelper/HYAdHelper.m
+++ HYApp_base/Utils/Helper/AdHelper/HYAdHelper.m
@@ -8,6 +8,32 @@
 
 #import "HYAdHelper.h"
 
+static NSString *const ad_image_name = @"AD_IMAGE_NAME";
+
 @implementation HYAdHelper
 
+#pragma mark - public methods
++ (BOOL)isNeedShow {
+    NSString *filePath = [self getFilePathWithImageName:[kUserDefaults valueForKey:adImageName]];
+    
+    BOOL isExist = [self isFileExistWithFilePath:filePath];
+}
+
+#pragma mark - private methods
+- (NSString *)adImagePathWithImageName:(NSString *)imageName {
+    if (imageName) {
+        // 获取Cache目录
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory,NSUserDomainMask, YES);
+        // 图片路径
+        NSString *filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:imageName];
+        return filePath;
+    }
+    return nil;
+}
+
+- (BOOL)isFileExistWithFilePath:(NSString *)filePath {
+    NSFileManager *fileManage = [NSFileManager defaultManager];
+    BOOL isDirectory = false;
+    
+}
 @end
